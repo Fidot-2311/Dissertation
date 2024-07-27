8094fb540706de5003d4a76c1e64e57eba8f78c8
Fidot-2311 <152402704+Fidot-2311@users.noreply.github.com>
2024-07-27 23:28:34 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -1,19 +1,6 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
-
-get_ipython().system('pip install flask joblib')
-
-
-# In[2]:
-
-
 from flask import Flask, request, jsonify
 import joblib
 import pandas as pd
-from threading import Thread
 
 # Load the trained XGBoost model
 model = joblib.load('best_xgboost_model.pkl')
@@ -53,59 +40,5 @@ def predict():
     except Exception as e:
         return jsonify({'error': str(e)})
 
-# Function to run the Flask app
-def run_app():
-    app.run(debug=True, use_reloader=False)
-
-# Run the Flask app in a separate thread
-thread = Thread(target=run_app)
-thread.start()
-
-
-# In[3]:
-
-
-get_ipython().system('pip install requests')
-
-
-# In[6]:
-
-
-import requests
-import json
-
-# Define the URL of the Flask API
-url = 'http://127.0.0.1:5000/predict'
-
-# Create a sample data input for prediction
-data = [{
-    "TouDef3rd": 3.20,
-    "TI": 9.31,
-    "Clr": 3.06,
-    "PasTotPrgDist": 2.77,
-    "PasDead": 4.69,
-    "TouDefPen": 3.40,
-    "PasMedCmp": 2.56,
-    "TouAtt3rd": 2.51,
-    "RecProg": 2.32,
-    "TouAttPen": 2.31,
-    "PasTotCmp": 2.62,
-    "TouMid3rd": 2.42,
-    "Shots": 2.22
-}]
-
-# Convert data to JSON
-data_json = json.dumps(data)
-
-# Send the POST request
-response = requests.post(url, json=data)
-
-# Print the response
-print(response.json())
-
-
-# In[ ]:
-
-
-
-
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
